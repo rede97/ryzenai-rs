a6d44445e00e06c2ebfd263587fe5efa8320c9df
rede97 <m454078356@163.com>
2025-03-05 10:19:45 +0800

    rm sdltest

--- yolov8/src/main.rs
+++ yolov8/src/main.rs
@@ -84,41 +84,6 @@ pub fn images_task<P: AsRef<Path>>(args: &args::Args, dir: P, model: Session) ->
 }
 
 fn camera_task(args: &args::Args, model: Session) -> Result<()> {
-    use sdl2::event::Event;
-    use sdl2::keyboard::Keycode;
-
-    let sdl_context = sdl2::init().unwrap();
-    let video_subsystem = sdl_context.video().unwrap();
-    let _window = video_subsystem
-        .window("Yolov8 on RyzenAI", 800, 600)
-        .opengl() // this line DOES NOT enable opengl, but allows you to create/get an OpenGL context from your window.
-        .build()
-        .unwrap();
-
-    // let mut canvas = window
-    //     .into_canvas()
-    //     .accelerated()
-    //     // .present_vsync()
-    //     .build()
-    //     .map_err(|e| e.to_string())
-    //     .map_err(|e| anyhow!("Create canvase error: {}", e))?;
-    let mut event_pump = sdl_context
-        .event_pump()
-        .map_err(|e| anyhow!("Create EventPump: {}", e))?;
-
-    'mainloop: loop {
-        for event in event_pump.poll_iter() {
-            match event {
-                Event::Quit { .. }
-                | Event::KeyDown {
-                    keycode: Option::Some(Keycode::Escape),
-                    ..
-                } => break 'mainloop,
-
-                _ => {}
-            }
-        }
-    }
     Ok(())
 }
 
